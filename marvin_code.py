c66ecff3032afafb5c93012f7dfc74f5d8a6ea14
RAYMONDE CENZANO <raymondecenzano@livingroommac.gateway.sonic.net>
2017-11-03 18:08:08 -0700

    name

--- marvin_code.py
+++ marvin_code.py
@@ -51,7 +51,7 @@ def marvin(data):
     if "where is" in data:
         data = data.split(" ")
         location = data[2]
-        speak("Hold on Frank, I will show you where " + location + " is.")
+        speak("Hold on Rafael, I will show you where " + location + " is.")
         os.system("chromium-browser https://www.google.nl/maps/place/" + location + "/&amp;")
  
 # initialization
