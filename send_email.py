3a708f7c902d97524ce55b3ad01d39dbf87f3634
RAYMONDE CENZANO <raymondecenzano@livingroommac.gateway.sonic.net>
2017-11-03 18:13:37 -0700

    new email sender

---
+++ send_email.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+#sending email must be gmail to use this port
+email_user = 'yourusername@gmail.com'
+email_receiver = 'receiver@domain.com'
+subject = 'Your subject here'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_receiver
+msg['Subject'] = subject
+
+#message area
+email_message = 'message that can be changed.\nMulti-Line'
+body = "Hello sent from program\n {}" .format(email_message)
+msg.attach(MIMEText(body,'plain'))
+text = msg.as_string()
+
+#sending code
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,'your password')
+
+server.sendmail(email_user,email_receiver,text)
+server.quit()
