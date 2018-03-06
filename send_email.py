5741dceac2744b314948b67e66c3093bee8e0aeb
SavageCoder77 <32753063+SavageCoder77@users.noreply.github.com>
2018-03-05 20:43:32 -0800

    added small changes
    added yahoo ports and links

--- send_email.py
+++ send_email.py
@@ -1,8 +1,9 @@
+#import
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-#sending email must be gmail to use this port
+#sending email must be gmail to use this script as it is right now
 email_user = 'yourusername@gmail.com'
 email_receiver = 'receiver@domain.com'
 subject = 'Your subject here'
@@ -20,8 +21,10 @@ text = msg.as_string()
 
 #sending code
 server = smtplib.SMTP('smtp.gmail.com',587)
+#'plus.smtp.mail.yahoo.com',465 for yahoo
 server.starttls()
 server.login(email_user,'your password')
 
 server.sendmail(email_user,email_receiver,text)
 server.quit()
+#done
