456316457cab2845b4f6d7b2548395915e7f1cb4
Savagecoder77 <savagecoder77@gmail.com>
2018-03-18 17:16:57 -0700

    added virtualenv wrapper code to scrpit

--- run_marvin.sh
+++ run_marvin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-source marvin/bin/activate
+source ~rafael/.bashrc
+workon marvin
 
-python2.7 marvin_code.py
+python2.7 /home/rafael/Documents/Programming/Marvin-Jarvis-/marvin_code.py
+
+deactivate
